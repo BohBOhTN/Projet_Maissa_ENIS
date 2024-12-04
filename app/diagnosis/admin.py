89982ec74cdeb791ba991cbbed045135cfd140a2
BohBOhTN <baha.etc@gmail.com>
2024-12-04 04:30:10 +0100

    Add admin panel and register models for users, questions, doctors, and diagnosis

--- app/diagnosis/admin.py
+++ app/diagnosis/admin.py
@@ -1,3 +1,10 @@
+# diagnosis/admin.py
 from django.contrib import admin
+from .models import Diagnosis
 
-# Register your models here.
+class DiagnosisAdmin(admin.ModelAdmin):
+    list_display = ('score_min', 'score_max', 'message')
+    search_fields = ('message',)
+
+# Register the model in the admin panel
+admin.site.register(Diagnosis)
--- app/doctors/admin.py
+++ app/doctors/admin.py
@@ -1,3 +1,10 @@
+# doctors/admin.py
 from django.contrib import admin
+from .models import Doctor
 
-# Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'location', 'latitude', 'longitude')
+    search_fields = ('first_name', 'last_name', 'email', 'location')
+
+# Register the model in the admin panel
+admin.site.register(Doctor)
